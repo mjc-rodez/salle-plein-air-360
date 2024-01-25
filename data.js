8bf4677771b61d025f6434569a2e1193186cd453
espnumrodez <157631492+espnumrodez@users.noreply.github.com>
2024-01-25 11:48:21 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salle-plein-air",
+      "name": "Salle PLEIN AIR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.2160412102253435,
+        "pitch": 0.043267511069505105,
+        "fov": 1.3337680586201845
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "salle-plein-air",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
